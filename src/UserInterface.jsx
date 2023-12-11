1f114d5418997d89826a8c98eddfbdeebfd3d8d2
jbucsa <justin.bucsa@gmail.com>
2023-12-11 15:09:31 -0600

    First Upload

---
+++ src/UserInterface.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react"
+import StylesContext from "./components/StylesContext"
+import CollectorButton from './CollectorButton'
+import SubmitForm from './SubmitForm'
+
+export default function UserInterface () {
+  const buttonStyling = React.useContext(StylesContext);
+
+  return (
+    <div>
+      <h2>We carry the latest and greatest editions of your favorite comic books!</h2>
+      <h2>Are you a collector?</h2>
+      <CollectorButton />
+
+      <h2>Have a special request? Click here to fill out our form</h2>
+      <SubmitForm />
+    </div>
+  )
+}
