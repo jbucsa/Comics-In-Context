ce78849b3c633527e6f440f399fbd62db7ccd1cc
jbucsa <justin.bucsa@gmail.com>
2023-12-11 15:28:18 -0600

    Cant resolve a Can't resolve './reportWebVitals' in 'C:\Users\Bucsa\class\ReactAndRedux\Comics-In-Context\src in code

--- src/components/RequestFormButton.jsx
+++ src/components/RequestFormButton.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
 
 export default function RequestFormButton() {
+  const buttonStyling = React.useContext(StylesContext);
+
   return (
     <div>
       <button style={buttonStyling}>Click Here!</button>
